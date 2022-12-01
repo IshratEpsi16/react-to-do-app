537945595c1dd3726495cf3083da9a1a9897bd8f
Ishrat Jahan Epsi <ishratepsi16@gmail.com>
2022-12-01 12:13:27 +0600

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      newItem: '',
+      list: []
+    }
+  }
+
+  //adding to do tasks
+  addItem() {
+    //create items with an unique id
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    }
+    //copy of current list
+    const list = [...this.state.list]
+    //adding item into list
+    list.push(newItem)
+    //update state
+    this.setState({
+      list,
+      newItem: ''
+
+    })
+
+  }
+  //updating list
+  updateList(key, value) {
+    this.setState({
+      [key]: value
+    })
+  }
+  //delete tasks
+  deleteItem(id) {
+    //copy of current list
+    const list = [...this.state.list]
+
+    const updatedList = list.filter(item => item.id !== id)
+    this.setState({
+      list: updatedList
+    })
+
+  }
+
+
+
+
+
+
+
+  render() {
+    return (
+      <div className='container'>
+        <h1 className='text-white mt-2 text-center'>TO DO List</h1>
+        <div className='container card mt-4' style={{ width: "40rem" }}>
+
+          <div className='card-body'>
+
+            <h3 className='text-secondary p-1'>Add your task</h3>
+
+            <input type='text' placeholder='Type your task' className=' form-control border border-secondary'
+              value={this.state.newItem}
+              onChange={e => this.updateList('newItem', e.target.value)}
+            />
+
+            <button className='btn btn-secondary mt-2' onClick={() => this.addItem()}>Add</button> <br />
+
+            <ol className='text-white bg-secondary mt-3'>
+
+              {this.state.list.map(item => {
+                return (
+                  <li key={item.id} >
+                    {item.value}
+
+                    <button className='m-2 btn btn-danger' onClick={() => this.deleteItem(item.id)}>x</button>
+                  </li>
+                );
+              })}
+
+            </ol>
+
+          </div>
+
+
+
+
+
+
+
+
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
